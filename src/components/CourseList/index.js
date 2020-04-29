d80ea0db34c7d1e717bb08067de8e40fc9becf36
Tiago Lima <tiago.lima@osf.digital>
2020-04-29 18:43:58 -0300

    .

---
+++ src/components/CourseList/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function addCourseAction(title) {
+    return {
+        type: 'ADD_COURSE',
+        title
+    }
+}
+
+export default function CourseList() {
+    const qty = 2;
+    const courses = useSelector(state => state.data.slice(0, qty), [qty]);
+    const dispatch = useDispatch();
+
+    function addCourse() {
+        dispatch(addCourseAction('Design Patterns'));
+    }
+
+    return (
+        <>
+            <ul>
+                {courses.map(course => <li key={course}>{course}</li>)}
+            </ul>
+            <button type="button" onClick={addCourse}>Adicionar curso</button>
+        </>
+    );
+}
